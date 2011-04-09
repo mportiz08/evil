a0fc4615ebb664abb1c7888ff7df0d25b41b8989
Marcus Ortiz <mportiz08@gmail.com>
2011-04-08 17:10:15 -0700

    added params to FuncType

--- FuncType.java
+++ FuncType.java
@@ -1,4 +1,10 @@
+import java.util.HashMap;
+
 public class FuncType extends Type
 {
-
+  public HashMap<String, Type> params;
+  public FuncType()
+  {
+    params = new HashMap<String, Type>();
+  }
 }
