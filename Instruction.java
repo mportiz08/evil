760613e801621d1f0774f4ff1e538f535e9a10ca
Marcus Ortiz <mportiz08@gmail.com>
2011-05-04 15:05:19 -0700

    added toSparc to Instruction

--- Instruction.java
+++ Instruction.java
@@ -6,4 +6,15 @@ public abstract class Instruction
   {
     this.name = new String(name);
   }
+  
+  public String toSparc()
+  {
+    return new String();
+  }
+  
+  // used to test instructions
+  public static void main(String[] args)
+  {
+    System.out.println(new ArithmeticInstruction("add", new Register(), new Register(), new Register()).toSparc());
+  }
 }
