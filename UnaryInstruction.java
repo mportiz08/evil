7c8a0754fd38b915b086457b49f8098d6839d05b
Jeffrey Good <jeff.d.good@gmail.com>
2011-05-06 15:52:37 -0700

    added tosparc to unary for del instruction

--- UnaryInstruction.java
+++ UnaryInstruction.java
@@ -1,11 +1,20 @@
 public class UnaryInstruction extends Instruction
 {
   public Register reg;
+  public Register sparcRegister;
   
   public UnaryInstruction(String name, Register reg)
   {
     super(name);
     this.reg = reg;
+    sparcRegister = new Register();
+  }
+  
+  public String toSparc(){
+    if(name.equals("del")){
+      return new String("mov " + reg + ", " + sparcRegister + "\n  call free\n  nop");
+    }
+    return "";
   }
   
   public String toString()
